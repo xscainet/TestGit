282f27476ed9e579b28d3e365f70a4ae993e5bf9
skylog <nikbrons@gmail.com>
2016-04-26 23:48:35 +0300

    add street

---
+++ Street.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Street
+    {
+
+        public House[] Houses = new House[10];
+
+        public int count = 0;
+
+        public void AddHouse(House h)
+        {
+            if (count < 10) Houses[count++] = h;
+        }
+
+        public void Draw()
+        {
+            for (int i=0;i<count;i++)
+            {
+                Houses[i].Draw();
+            }
+        }
+
+    }
+}
